afe52c223cc8b7d7c1dee22da502e4c38ac67b5a
dr-spangle <dr-spangle@dr-spangle.com>
2015-05-29 22:24:36 +0100

    Started work on Input class. Currently just has a static techSupportOath method.

---
+++ Input.py
@@ -0,0 +1,31 @@
+'''
+Created on 29 May 2015
+
+@author: dr-spangle
+'''
+
+class Input(object):
+    '''
+    Stores a bunch of different raw inputs to be used.
+    '''
+
+    @staticmethod
+    def techSupportOath():
+        '''
+        Recites the tech support oath.
+        Parody of the Night's Watch oath.
+        '''
+        output = "User issues gather, and now my watch begins." 
+        output += "It shall not end until my death. "
+        output += "I shall take no wife (that I will ever see except on weekends), "
+        output += "hold no lands (because I don't make nearly enough), "
+        output += "father no children (because I will never be home anyway). "
+        output += "I shall receive no thanks and win no glory. "
+        output += "I shall live and die at my desk. "
+        output += "I am the antivirus in the darkness. "
+        output += "I am the coder on the walls. "
+        output += "I am the password reset that guards the logins of men. "
+        output += "I pledge my life and honor to the Help Desk's Watch, "
+        output += "for this night and all the nights to come."
+        return output
+        
