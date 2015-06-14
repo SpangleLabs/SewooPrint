773731bd57e92ee594b9942d7ef2100bf9e2f5b2
joshcoales <joshua@coales.co.uk>
2015-06-14 14:53:12 +0100

    Removing an old temp file

--- oathTest.py
+++
@@ -1,8 +0,0 @@
-import Formatting
-import Input
-import Printer
-
-text = Input.Input.techSupportOath()
-formatText = Formatting.Formatting.multiLine(text)
-eFormatText = formatText.encode()
-Printer.Printer.print_raw(eFormatText)
