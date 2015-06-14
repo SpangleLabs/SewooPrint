8337d7e2b4bb9ff84bbdba94c74f510e8b53e5ef
joshcoales <joshua@coales.co.uk>
2015-06-14 14:22:03 +0100

    Fixing some bugs in Formatter

--- Formatter.py
+++ Formatter.py
@@ -29,14 +29,14 @@ class Formatter:
         return "\n".join(textLines)
 
     def tryEncode(self,text):
-        'Tries to encode string to bytes, unless it's already bytes.'
+        'Tries to encode string to bytes, unless it\'s already bytes.'
         try:
-            bytes = text.encode()
-            return bytes
+            encodeText = text.encode()
+            return encodeText
         except AttributeError:
-            return bytes
+            return text
 
     def bold(self,text):
         'Makes a set string bold.'
-        bytes = self.tryEncode(text)
-        return b'\x1b\x21\x08' + bytes + b'\x1b\x21\x00'
+        encodeText = self.tryEncode(text)
+        return b'\x1b\x21\x08' + encodeText + b'\x1b\x21\x00'
