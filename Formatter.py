c0b5be9760b7993aa0276704da7f84002d727104
Joshua Coales <joshua@coales.co.uk>
2015-06-14 13:03:23 +0000

    Added tryEncode() and bold() methods to formatter

--- Formatter.py
+++ Formatter.py
@@ -28,3 +28,15 @@ class Formatter:
         textLines.append(line)
         return "\n".join(textLines)
 
+    def tryEncode(self,text):
+        'Tries to encode string to bytes, unless it's already bytes.'
+        try:
+            bytes = text.encode()
+            return bytes
+        except AttributeError:
+            return bytes
+
+    def bold(self,text):
+        'Makes a set string bold.'
+        bytes = self.tryEncode(text)
+        return b'\x1b\x21\x08' + bytes + b'\x1b\x21\x00'
