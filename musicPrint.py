fcd2ecf000881dc453d363213783df1229e187e1
Joshua Coales <joshua@coales.co.uk>
2019-11-06 17:29:11 +0000

    Neatening up a lot of very old code

--- musicPrint.py
+++ musicPrint.py
@@ -1,57 +1,55 @@
 from PIL import Image
 import math
 
-musicImage = "/home/dr-spangle/music_image.png"
+music_image = "/home/dr-spangle/music_image.png"
 
 f = open("/dev/usb/lp1", "wb")
 
-if(True):
-    def loadBigImageFile(fileName):
-        image = Image.open(fileName).convert('RGBA')
-        width,height = image.size
-        newwidth = 400
-        print(width)
-        print(height)
-        print(newwidth)
-        print(str(width) + "/" + str(newwidth))
-        scalefactor = width/(newwidth+0.0)
-        print(scalefactor)
-        newheight = height//scalefactor
-        endwidth = math.ceil(newwidth/8)*8
-        endheight = math.ceil(newheight/8)*8
-        image = image.resize((endwidth,endheight),Image.ANTIALIAS)
-        
-        numSegments = endheight//400
-        print(numSegments)
-        image_string = b''
-        for segmentNum in range(numSegments):
-            offset = segmentNum*400
-            segHeight = min(400,endheight)
 
-            image_string = b'\x1d\x2a'
-            image_string += bytes([endwidth//8,segHeight//8])
-            pixnum = 0
-            pixval = 0
-            for x in range(endwidth):
-                for y in range(segHeight):
-                    r,g,b,a = image.getpixel((x,y+offset))
-                    if(r*g*b<100*100*100 and a>50):
-                        pixval += 2**(7-pixnum)
-                    if(pixnum==7):
-                        image_string += bytes([pixval])
-                        pixnum = 0
-                        pixval = 0
-                    else:
-                        pixnum += 1
-            image_string += b'\x1d\x2f\x00'
-            f.write(image_string)
-        return image_string
+def load_big_image_file(file_name):
+    image = Image.open(file_name).convert('RGBA')
+    width, height = image.size
+    new_width = 400
+    print(width)
+    print(height)
+    print(new_width)
+    print(str(width) + "/" + str(new_width))
+    scale_factor = width / (new_width + 0.0)
+    print(scale_factor)
+    new_height = height // scale_factor
+    end_width = math.ceil(new_width / 8) * 8
+    end_height = math.ceil(new_height / 8) * 8
+    image = image.resize((end_width, end_height), Image.ANTIALIAS)
 
-image_string = b""
-loadBigImageFile(musicImage)
+    num_segments = end_height // 400
+    print(num_segments)
+    output = b''
+    for segmentNum in range(num_segments):
+        offset = segmentNum * 400
+        seg_height = min(400, end_height)
+
+        output = b'\x1d\x2a'
+        output += bytes([end_width // 8, seg_height // 8])
+        pix_num = 0
+        pix_val = 0
+        for x in range(end_width):
+            for y in range(seg_height):
+                r, g, b, a = image.getpixel((x, y + offset))
+                if r * g * b < 100 * 100 * 100 and a > 50:
+                    pix_val += 2 ** (7 - pix_num)
+                if pix_num == 7:
+                    output += bytes([pix_val])
+                    pix_num = 0
+                    pix_val = 0
+                else:
+                    pix_num += 1
+        output += b'\x1d\x2f\x00'
+        f.write(output)
+    return output
+
+
+image_string = load_big_image_file(music_image)
 image_string += b'\n\n\n\n\n\n\x1d\x56\x01\n'
 
-
 f.write(image_string)
-#print(image_string)
-
+# print(image_string)
