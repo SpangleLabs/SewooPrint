c54884985f4814830b01bc55cc4ce437a5215958
joshcoales <joshua@coales.co.uk>
2019-11-24 15:50:17 +0000

    Adding a web server

---
+++ web_server.py
@@ -0,0 +1,36 @@
+import json
+
+import flask
+from flask import request, abort
+
+from input_web import ChoresBoardDocument
+from printer_repo import PrinterRepo
+
+app = flask.Flask(__name__)
+
+with open("config.json", "r") as f:
+    CONFIG = json.load(f)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello, this is the printer server."
+
+
+@app.route("/", methods=["POST"])
+def print_doc():
+    data = request.json
+    if 'key' not in data or data['key'] != CONFIG['web_key']:
+        abort(401)
+        return
+    doc = data['document'].strip()
+    if doc.startswith("the "):
+        doc = doc[4:]
+    if doc.startswith("my "):
+        doc = doc[3:]
+    if doc in ['chores board', 'chores', 'choresboard', 'chores list']:
+        printer = PrinterRepo().default_printer()
+        chores_doc = ChoresBoardDocument()
+        printer.print_document(chores_doc)
+        return "Printed chores document"
+    return f"Unknown document {doc}"
