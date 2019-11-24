927f873ce1579cd57f755a441f1e4f6f72d4a4e5
joshcoales <joshua@coales.co.uk>
2019-11-24 15:12:10 +0000

    Neatening up ChoresBoardDocument class, and fixing nealine after None last done date. Hiding next date if no recommended period is set

--- input_web.py
+++ input_web.py
@@ -1,4 +1,5 @@
 import json
+from typing import List, Dict, Optional
 
 import isodate
 import requests
@@ -14,27 +15,34 @@ class ChoresBoardDocument(TextDocument):
             config = json.load(f)
         dailys_url = config['dailys_url']
         chores_url = f"{dailys_url}/views/chores_board.json"
-        chores_data = requests.get(chores_url).json()
+        self.chores_data = requests.get(chores_url).json()
         self.add_title("Chores board")
-        self.add_bold_text("Date: ").add_text(chores_data['today']).nl()
-        for category in chores_data['chores']:
-            self.add_bold_centered_text(category).nl()
-            for chore in chores_data['chores'][category]:
-                self.add_bold_text(chore['display_name']).nl()
-                # Add latest done date, invert if over two months
-                self.add_bold_text("Last done: ")
-                if chore['latest_done'] is None:
-                    self.add_invert_colours_text("-")
-                else:
-                    days = isodate.parse_date(chores_data['today']) - isodate.parse_date(chore['latest_done'])
-                    if days.days > 60 and chore['recommended_period'] is None:
-                        self.add_invert_colours_text(chore['latest_done']).nl()
-                    else:
-                        self.add_text(chore['latest_done']).nl()
-                # Add next date, invert if overdue
-                self.add_bold_text("Next date: ")
-                if chore['is_overdue']:
-                    self.add_invert_colours_text(chore['next_date']).nl()
-                else:
-                    self.add_text(chore['next_date']).nl()
-                self.nl()
+        self.add_bold_text("Date: ").add_text(self.chores_data['today']).nl()
+        for category, chores_list in self.chores_data['chores'].items():
+            self._add_category(category, chores_list)
+
+    def _add_category(self, category: str, chores_list: List[Dict[str, Optional[str]]]):
+        self.add_bold_centered_text(category.capitalize()).nl()
+        for chore in chores_list:
+            self._add_chore(chore)
+
+    def _add_chore(self, chore: Dict[str, Optional[str]]):
+        self.add_bold_text(chore['display_name']).nl()
+        # Add latest done date, invert if over two months
+        self.add_bold_text("Last done: ")
+        if chore['latest_done'] is None:
+            self.add_invert_colours_text("None").nl()
+        else:
+            days = isodate.parse_date(self.chores_data['today']) - isodate.parse_date(chore['latest_done'])
+            if days.days > 60 and chore['recommended_period'] is None:
+                self.add_invert_colours_text(chore['latest_done']).nl()
+            else:
+                self.add_text(chore['latest_done']).nl()
+        # Add next date, invert if overdue
+        if chore['recommended_period'] is not None:
+            self.add_bold_text("Next date: ")
+            if chore['is_overdue']:
+                self.add_invert_colours_text(chore['next_date']).nl()
+            else:
+                self.add_text(chore['next_date']).nl()
+            self.nl()
