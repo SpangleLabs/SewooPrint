44d21e1bcd9b303c0154ccf369fdf6c7a21684da
joshcoales <joshua@coales.co.uk>
2019-11-24 15:00:28 +0000

    Stringify all text before encoding

--- document.py
+++ document.py
@@ -5,7 +5,7 @@ from enum import Enum
 def _try_encode(text):
     """Tries to encode string to bytes, unless it is already bytes."""
     try:
-        text_encode = text.encode().replace(b'\xc2\xa3', b'\x9c')
+        text_encode = str(text).encode().replace(b'\xc2\xa3', b'\x9c')
         return text_encode
     except AttributeError:
         return text
