34c6c52b66bcc4781be8ef842615a6af43ca727e
Joshua Coales <joshua@coales.co.uk>
2019-12-22 14:20:23 +0000

    Adding Requests classes for all text and web requests in input.py, converting it to use them

--- input.py
+++ input.py
@@ -1,7 +1,8 @@
 from document_image import GreyScaleImage, SilhouetteImage
 from document_web import ChoresBoardDocument
 from printer_repo import PrinterRepo
-from document import TextDocument
+from requests import TechSupportOathRequest, NotMyBusinessRequest, HAL9000WarningRequest, RawTextRequest, \
+    ChoresBoardRequest
 
 
 # noinspection PyMethodMayBeStatic
@@ -16,8 +17,6 @@ class Input(object):
     TYPE_STREAM = "stream"
     TYPE_WEB = "web"
 
-    mPrinter = None
-
     def __init__(self):
         """
         Start input choice.
@@ -30,7 +29,8 @@ class Input(object):
         if input_type == self.TYPE_TEXT:
             print("Text input type selected.")
             # Handle text type input.
-            self.choose_text_input()
+            requests = [TechSupportOathRequest(), NotMyBusinessRequest(), HAL9000WarningRequest(), RawTextRequest()]
+            self.choose_input(requests)
             return
         if input_type == self.TYPE_ASCII:
             print("Ascii art input type selected.")
@@ -47,7 +47,8 @@ class Input(object):
             return
         if input_type == self.TYPE_WEB:
             print("Web input type selected.")
-            self.choose_web_input()
+            requests = [ChoresBoardRequest()]
+            self.choose_input(requests)
             return
         print("Unknown input type.")
         return
@@ -72,26 +73,16 @@ class Input(object):
             return self.TYPE_WEB
         return None
 
-    def choose_text_input(self):
+    def choose_input(self, requests):
         """chooses a text input"""
-        print("Available text inputs: tech support oath, not my business, HAL9000 warning, raw.")
+        print("Available inputs: " + ", ".join(x.name for x in requests))
         user_input = input("Please select: ")
         user_input_clean = user_input.lower().strip()
-        if user_input_clean in ['tech support oath', 'techsupportoath', 'oath']:
-            oath_document = self.tech_support_oath()
-            self.mPrinter.print_document(oath_document)
-            print("Printing complete")
-            return
-        if user_input_clean in ['not my business', 'notmybusiness']:
-            poem_document = self.not_my_business()
-            self.mPrinter.print_document(poem_document)
-            print("Printing complete")
-            return
-        if user_input_clean in ['hal9000 warning', 'hal warning', 'hal', 'hal9000']:
-            warn_document = self.hal_warning()
-            self.mPrinter.print_document(warn_document)
-            print("Printing complete")
-            return
+        for request in requests:
+            if request.matches_input(user_input_clean):
+                print("Printing " + request.name)
+                request.print(self.mPrinter)
+                return
         print("I don't know that one.")
 
     def choose_image_input(self):
@@ -112,88 +103,6 @@ class Input(object):
             print("Printing complete")
             return
         print("I don't know that one.")
-
-    def choose_web_input(self):
-        """chooses a web input"""
-        print("Available web inputs: chores board.")
-        user_input = input("Please select: ")
-        user_input_clean = user_input.lower().strip()
-        if user_input_clean in ['chores board', 'chores', 'choresboard', 'chores list']:
-            chores_document = ChoresBoardDocument()
-            self.mPrinter.print_document(chores_document)
-            print("Printing complete")
-            return
-        print("I don't know that one.")
-
-    def tech_support_oath(self):
-        """
-        Recites the tech support oath.
-        Parody of the Night's Watch oath.
-        """
-        output = """\
-User issues gather, and now my watch begins.
-It shall not end until my death.
-I shall take no wife (that I will ever see except on weekends),
-hold no lands (because I don't make nearly enough),
-father no children (because I will never be home anyway).
-I shall receive no thanks and win no glory.
-I shall live and die at my desk.
-I am the antivirus in the darkness.
-I am the coder on the walls.
-I am the password reset that guards the logins of men.
-I pledge my life and honor to the Help Desk's Watch,
-for this night and all the nights to come."""
-        document = TextDocument().add_title("Tech support oath")
-        document.add_line_wrapped_text(output)
-        return document
-
-    def not_my_business(self):
-        """
-        Outputs Niyi Osundere's "Not my business"
-        """
-        poem = """ \
-They picked Akanni up one morning
-Beat him soft like clay
-And stuffed him down the belly
-Of a waiting jeep.
-What business of mine is it
-So long they don't take the yam
-From my savouring mouth?
-        
-They came one night
-Booted the whole house awake
-And dragged Danladi out,
-Then off to a lengthy absence.
-What business of mine is it
-So long they don't take the yam
-From my savouring mouth?
-
-Chinwe went to work one day
-Only to find her job was gone:
-No query, no warning, no probe -
-Just one neat sack for a stainless record.
-What business of mine is it
-So long they don't take the yam
-From my savouring mouth?
-
-And then one evening
-As I sat down to eat my yam
-A knock on the door froze my hungry hand.
-The jeep was waiting on my bewildered lawn
-Waiting, waiting in its usual silence."""
-        document = TextDocument().add_title("Not My Business")
-        document.add_title("by Niyi Osundere")
-        document.add_line_wrapped_text(poem)
-        return document
-
-    def hal_warning(self):
-        """
-        Outputs HAL9000's warning from the end of 2010:Odyssey Two
-        """
-        document = TextDocument()\
-            .add_bold_centered_text("ALL THESE WORLDS ARE YOURS-EXCEPT EUROPA").nl()\
-            .add_bold_centered_text("ATTEMPT NO LANDING THERE")
-        return document
 
     @staticmethod
     def load_image_file(file_name):
---
+++ requests.py
@@ -0,0 +1,162 @@
+from abc import ABC, abstractmethod
+
+from document import TextDocument
+from document_web import ChoresBoardDocument
+from printer import Printer
+
+
+class Request(ABC):
+
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        pass
+
+    @abstractmethod
+    def matches_input(self, user_input: str) -> bool:
+        pass
+
+    @abstractmethod
+    def print(self, printer: Printer):
+        pass
+
+
+class PartialRequest(Request, ABC):
+
+    @abstractmethod
+    def get_more_info(self):
+        pass
+
+
+class TechSupportOathRequest(Request):
+
+    @property
+    def name(self) -> str:
+        return "Tech support oath"
+
+    def matches_input(self, user_input: str) -> bool:
+        return user_input in ['tech support oath', 'techsupportoath', 'oath']
+
+    def print(self, printer: Printer):
+        """
+        Recites the tech support oath.
+        Parody of the Night's Watch oath.
+        """
+        output = """\
+User issues gather, and now my watch begins.
+It shall not end until my death.
+I shall take no wife (that I will ever see except on weekends),
+hold no lands (because I don't make nearly enough),
+father no children (because I will never be home anyway).
+I shall receive no thanks and win no glory.
+I shall live and die at my desk.
+I am the antivirus in the darkness.
+I am the coder on the walls.
+I am the password reset that guards the logins of men.
+I pledge my life and honor to the Help Desk's Watch,
+for this night and all the nights to come."""
+        document = TextDocument().add_title("Tech support oath")
+        document.add_line_wrapped_text(output)
+        printer.print_document(document)
+
+
+class NotMyBusinessRequest(Request):
+
+    @property
+    def name(self) -> str:
+        return "Not My Business"
+
+    def matches_input(self, user_input: str) -> bool:
+        return user_input in ['not my business', 'notmybusiness']
+
+    def print(self, printer: Printer):
+        """
+        Outputs Niyi Osundere's "Not my business"
+        """
+        poem = """ \
+They picked Akanni up one morning
+Beat him soft like clay
+And stuffed him down the belly
+Of a waiting jeep.
+What business of mine is it
+So long they don't take the yam
+From my savouring mouth?
+        
+They came one night
+Booted the whole house awake
+And dragged Danladi out,
+Then off to a lengthy absence.
+What business of mine is it
+So long they don't take the yam
+From my savouring mouth?
+
+Chinwe went to work one day
+Only to find her job was gone:
+No query, no warning, no probe -
+Just one neat sack for a stainless record.
+What business of mine is it
+So long they don't take the yam
+From my savouring mouth?
+
+And then one evening
+As I sat down to eat my yam
+A knock on the door froze my hungry hand.
+The jeep was waiting on my bewildered lawn
+Waiting, waiting in its usual silence."""
+        document = TextDocument().add_title("Not My Business")
+        document.add_title("by Niyi Osundere")
+        document.add_line_wrapped_text(poem)
+        printer.print_document(document)
+
+
+class HAL9000WarningRequest(Request):
+
+    @property
+    def name(self) -> str:
+        return "HAL9000 warning"
+
+    def matches_input(self, user_input: str) -> bool:
+        return user_input in ['hal9000 warning', 'hal warning', 'hal', 'hal9000']
+
+    def print(self, printer: Printer):
+        """
+        Outputs HAL9000's warning from the end of 2010:Odyssey Two
+        """
+        document = TextDocument()\
+            .add_bold_centered_text("ALL THESE WORLDS ARE YOURS-EXCEPT EUROPA").nl()\
+            .add_bold_centered_text("ATTEMPT NO LANDING THERE")
+        printer.print_document(document)
+
+
+class RawTextRequest(Request):
+
+    def __init__(self):
+        self.text = None
+
+    @property
+    def name(self) -> str:
+        return "raw"
+
+    def matches_input(self, user_input: str) -> bool:
+        if user_input.startswith("raw"):
+            self.text = user_input[3:].strip(" :")
+            return True
+        return False
+
+    def print(self, printer: Printer):
+        document = TextDocument().add_text(self.text)
+        printer.print_document(document)
+
+
+class ChoresBoardRequest(Request):
+
+    @property
+    def name(self) -> str:
+        return "Chores board"
+
+    def matches_input(self, user_input: str) -> bool:
+        return user_input in ['chores board', 'chores', 'choresboard', 'chores list']
+
+    def print(self, printer: Printer):
+        chores_document = ChoresBoardDocument()
+        printer.print_document(chores_document)
